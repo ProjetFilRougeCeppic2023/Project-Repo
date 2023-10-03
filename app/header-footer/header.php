b20fda58bc2d810aa599e030c61f8bbedb290b42
tgabor1 <t.gabor@tutanota.com>
2023-10-03 13:57:53 +0200

    Premier dépôt

--- app/header-footer/header.php
+++ app/header-footer/header.php
@@ -1,3 +1,29 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-echo 'header';
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Header</title>
+    <link rel="stylesheet" href="../css/common_style.css">
+</head>
+
+<body>
+    <header>
+        <div id="logo">
+            <img src="" alt="Logo du site">
+        </div>
+
+        <div class="recherche">
+            <input type="text" id="search" placeholder="Rechercher...">
+            <button type="submit">Rechercher</button>
+        </div>
+
+        <div id="compte">
+            <a href="../">Se connecter</a>
+            <a href="../">S'inscrire</a>
+        </div>
+    </header>
+</body>
+
+</html>
