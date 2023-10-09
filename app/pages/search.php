6c80ae36cf60717b48ed24fd47b24ef4f4866c67
nocared <paul.richarddu30@gmail.com>
2023-10-09 15:15:12 +0200

    ADD - Creation movie grid + changement file name

--- app/pages/search.php
+++ app/pages/search.php
@@ -4,8 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/common_style.css">
-    <link rel="stylesheet" href="../css/search_style.css">
+    <link rel="stylesheet" href="../css/search.css">
     <title>Search</title>
 </head>
 
@@ -15,15 +14,39 @@
     </header>
 
 
-    <div id="searchSpace">
+    <section id="searchSpace">
         <form role="search">
             <input id="search" type="search" placeholder="Search..." />
         </form>
-    </div>
+    </section>
+
+
+    <section id="movieGrid">
+        <div class="movie">
+            <div class="picture">
+                <img src="" alt="Film picture">
+            </div>
+            <h2>Title</h2>
+            <div class="specifications">
+                <ul class="themesList">
+                    <li>Theme1</li>
+                    <li>Theme2</li>
+                    <li>Theme3</li>
+                </ul>
+                <div class="stats">
+                    <div class="upvote">128</div>
+                    <div class="favorites">O</div>
+                </div>
+
+            </div>
+        </div>
 
 
 
-    
+    </section>
+
+
+
     <footer>
         <?php require_once('../header-footer/footer.php') ?>
     </footer>
