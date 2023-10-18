d0114fc54a768f87511013b2412a027687f75b09
nocared <paul.richarddu30@gmail.com>
2023-10-18 16:05:45 +0200

    ADD - Links controllers and views

--- src/controllers/user-board.php
+++ src/controllers/user-board.php
@@ -1,28 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/commonStyle.css">
-    <link rel="stylesheet" href="../css/user-board_style.css">
-    <title>Board</title>
-</head>
-
-<body>
-    <header>
-        <?php require_once('./header-footer/header.php') ?>
-    </header>
-
-
-
-
-
-    
-    <footer>
-        <?php require_once('./header-footer/footer.php') ?>
-    </footer>
-
-</body>
-
-</html>
+include 'views/user-board.views.php';
