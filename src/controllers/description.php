908bf9c2ca6c31543b2d140fe4c3351f100dbbc8
nocared <paul.richarddu30@gmail.com>
2023-10-23 16:36:11 +0200

    ADD - Click on moving bring to movie description

--- src/controllers/description.php
+++ src/controllers/description.php
@@ -1,6 +1,31 @@
 <?php
 
+
+require 'movie.php';
+
+// $db = new Database();
+$id = $_GET['id'];
+// $queryArticle = 'SELECT * FROM post where id = :id';
+// $article = $db->query($queryArticle, [':id' => $id])->find();
+
+$movie = null;
+
+for ($i = 0; $i < count($currentFilms);$i++)
+{
+    if ($currentFilms[$i]->id === $id )
+    {
+        $movie = $currentFilms[$i];
+        break;
+    }
+}
+
+if (! $movie) {
+    abort();
+}
+else
+{
+    echo $movie->name;
+}
+
 include 'views/description.views.php';
-
-
 ?>
--- src/views/search.views.php
+++ src/views/search.views.php
@@ -1,70 +1,61 @@
-<!DOCTYPE html>
-<html lang="en">
+<header>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/search_style.css">
-    <script type="module" src="../scripts/search/scriptSearch.js" defer></script>
-    <title>Search</title>
-</head>
-
-<body>
-    <header>
-
-        <?php require('views/partials/header.php') ?>
-    </header>
+    <?php require('views/partials/header.php') ?>
+</header>
 
 
-    <section id="searchSpace">
-        <form role="search">
-            <input id="search" type="search" placeholder="Search..." />
-        </form>
-    </section>
+<section id="searchSpace">
+    <form role="search">
+        <input id="search" type="search" placeholder="Search..." />
+    </form>
+</section>
 
 
-    <section id="movieGrid">
+<section id="movieGrid">
 
-        <?php foreach ($currentFilms as $currentFilm) { ?>
+    <?php foreach ($currentFilms as $currentFilm) { ?>
 
-            <div class="movie">
-                <div class="picture">
-                    <img src="<?= $currentFilm->icon ?>" alt="Film picture">
-                </div>
 
+        <div class="movie">
+            <div class="picture">
+                <img src="<?= $currentFilm->icon ?>" alt="Film picture">
+            </div>
+
+            <a href="/description?id=<?= $currentFilm->id ?>">
                 <div class="title">
                     <h2><?= $currentFilm->name ?></h2>
                 </div>
+            </a>
 
-                <div class="specifications">
-                    <ul class="themesList">
-                        <?php foreach ($currentFilm->themes as $theme) { ?>
-                            <li><?= $theme ?></li>
-                        <?php } ?>
-                    </ul>
+            <div class="specifications">
+                <ul class="themesList">
+                    <?php foreach ($currentFilm->themes as $theme) { ?>
+                        <li><?= $theme ?></li>
+                    <?php } ?>
+                </ul>
 
-                    <div class="stats">
-                        <div class="upvote">
-                            <span><?= $currentFilm->rating ?></span>
-                            <div>
-                                <img src="assets/images/heart.png" alt="icon vote">
-                            </div>
-                        </div>
-                        <div class="favorites">
-                            <div>
-                                <img src=<?= $currentFilm->isStar ? "assets/images/star_fill.png" : "assets/images/star_empty.png" ?>  alt="favorite button">
-                            </div>
+                <div class="stats">
+                    <div class="upvote">
+                        <span><?= $currentFilm->rating ?></span>
+                        <div>
+                            <img src="assets/images/heart.png" alt="icon vote">
                         </div>
                     </div>
-
+                    <div class="favorites">
+                        <div>
+                            <img src=<?= $currentFilm->isStar ? "assets/images/star_fill.png" : "assets/images/star_empty.png" ?> alt="favorite button">
+                        </div>
+                    </div>
                 </div>
+
             </div>
+        </div>
 
-        <?php } ?>
-    </section>
+    <?php } ?>
+</section>
 
 
 
-    <footer>
-        <?php require('views/partials/footer.php') ?>
-    </footer>
+<footer>
+    <?php require('views/partials/footer.php') ?>
+</footer>
