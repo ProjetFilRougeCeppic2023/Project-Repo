457efd424fede9d5a0f180e8a6158a4d7b462e55
nocared <paul.richarddu30@gmail.com>
2023-12-08 11:34:05 +0100

    ADD - Fixtures User

--- DEY/src/DataFixtures/UserFixtures.php
+++ DEY/src/DataFixtures/UserFixtures.php
@@ -2,29 +2,28 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Movie;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
 
 ;
 
-class MovieFixtures extends Fixture
+class UserFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create();
         // Utilisez $faker pour générer des données aléatoires
         for ($i = 0; $i < 15; $i++) {
-            $movie = new Movie();
-            $movie->setName($faker->name);
-            $movie->setDescription($faker->paragraph(5));
-            $movie->setThemes(implode(',', $faker->words(5)));
+            $movie = new User();
+            $movie->setUsername($faker->name);
+            $movie->setRoles(['ROLE_USER']);
+            $movie->setPassword('motdepasse');
+            $movie->setPassword($faker->email());
 
             $manager->persist($movie);
         }
-
-
         $manager->flush();
     }
 }
