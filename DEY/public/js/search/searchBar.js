ba161d41f31db04c0e7aef11ee316d9a7c3c3297
nocared <paul.richarddu30@gmail.com>
2023-12-19 10:04:08 +0100

    ADD - Search in an other file + modify css tag

---
+++ DEY/public/js/search/searchBar.js
@@ -0,0 +1,49 @@
+// // Votre JavaScript pour la barre de recherche
+//         // Utilisez jQuery pour simplifier les requêtes Ajax
+//         $(document).ready(function() {
+//             var searchInput = $('#search-input');
+//             var searchResults = $('#search-results');
+
+//             searchInput.on('input', function() {
+//                 // Récupérez la valeur de la recherche
+//                 var query = searchInput.val();
+
+//                 // Envoyez une requête au serveur Symfony via Ajax
+//                 $.ajax({
+//                      url: '{{ path('app_movie_search') }}',
+//                     method: 'GET',
+//                     data: { query: query },
+//                     dataType: 'json',
+//                     success: function(data) {
+//                         // Mettez à jour les résultats de la recherche dans l'interface utilisateur
+//                         // Vous pouvez personnaliser cela en fonction de la structure des résultats de votre recherche
+//                         searchResults.empty();
+//                         data.results.forEach(function(result) {
+//                             searchResults.append('<li>' + result.name + '</li>');
+//                         });
+//                     }
+//                 });
+//             });
+//         });
+document.addEventListener('DOMContentLoaded', function () {
+    var searchInput = document.getElementById('search-input');
+    var searchResults = document.getElementById('search-results');
+    var searchUrl = document.getElementById('search-bar').getAttribute('data-search-url');
+
+    searchInput.addEventListener('input', function () {
+        var query = searchInput.value;
+
+        fetch(searchUrl + '?query=' + encodeURIComponent(query))
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                searchResults.innerHTML = '';
+                data.results.forEach(function (result) {
+                    var li = document.createElement('li');
+                    li.textContent = result.title;
+                    searchResults.appendChild(li);
+                });
+            });
+    });
+});
