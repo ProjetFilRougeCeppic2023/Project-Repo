949f8cbcdd5160f0a3b5617c3f962ca120997a1f
nocared <paul.richarddu30@gmail.com>
2023-12-22 16:17:42 +0100

    ADD - Some movies

--- DEY/src/DataFixtures/MovieFixtures.php
+++ DEY/src/DataFixtures/MovieFixtures.php
@@ -18,8 +18,37 @@ class MovieFixtures extends Fixture implements DependentFixtureInterface
         $faker = Factory::create();
         // Load tags from the database
         $tags = $manager->getRepository(Tag::class)->findAll();
+
+
+        $movieData = [
+            ['title' => 'The Shawshank Redemption', 'description' => 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.', 'themes' => 'Drama', 'tags' => ['Drama']],
+            ['title' => 'The Godfather', 'description' => 'The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.', 'themes' => 'Crime, Drama', 'tags' => ['Crime', 'Drama']],
+            
+        ];
+        
+        foreach ($movieData as $data) {
+            $movie = new Movie();
+            $movie->setName($data['title']);
+            $movie->setDescription($data['description']);
+            $movie->setThemes($data['themes']);
+        
+            $movie->setCreationDateValue();
+        
+            // Ajoutez les tags spécifiques à chaque film
+            foreach ($data['tags'] as $tagName) {
+                $tag = $manager->getRepository(Tag::class)->findOneBy(['name' => $tagName]);
+                if ($tag) {
+                    $movie->addTag($tag);
+                }
+            }
+        
+            $manager->persist($movie);
+        }
+
+
+        ///////////////////////////////////////////////////////////////////
         // Utilisez $faker pour générer des données aléatoires
-        for ($i = 0; $i < 15; $i++) {
+        for ($i = 0; $i < 100; $i++) {
             $movie = new Movie();
             $movie->setName($faker->name);
             $movie->setDescription($faker->paragraph(5));
